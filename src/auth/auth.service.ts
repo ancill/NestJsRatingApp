a170f57031740ab1561632beaf86c6fd202b6b10
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2022-03-24 00:21:48 +0300

    auth done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,9 +5,13 @@ import { AuthDto } from './dto/auth.dto';
 import { UserModel } from './user.model';
 import { genSalt, hash, compare } from 'bcryptjs';
 import { USER_NOT_FOUND, WRONG_PASSWORD_ERROR } from './auth.constants';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
-	constructor(@InjectModel(UserModel) private readonly userModel: ModelType<UserModel>) {}
+	constructor(
+		@InjectModel(UserModel) private readonly userModel: ModelType<UserModel>,
+		private readonly jwtService: JwtService,
+	) {}
 
 	async createUser(dto: AuthDto) {
 		const salt = await genSalt(10);
@@ -38,7 +42,7 @@ export class AuthService {
 	async login(email: string) {
 		const payload = { email };
 		return {
-			access_token: 
-		}
+			access_token: await this.jwtService.signAsync(payload),
+		};
 	}
 }
