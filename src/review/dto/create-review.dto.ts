dddd9e10162a95bfd8f75aa954ac2a61dcd1b6a1
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2022-03-28 00:43:59 +0300

    1

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,7 +1,20 @@
+import { IsNumber, IsString, isString, Max, Min } from 'class-validator';
+
 export class CreateReviewDto {
+	@IsString()
 	name: string;
+
+	@IsString()
 	title: string;
+
+	@IsString()
 	description: string;
+
+	@Max(5)
+	@Min(1, { message: 'Rating cannot be less then 1' })
+	@IsNumber()
 	rating: number;
+
+	@IsString()
 	productId: string;
 }
